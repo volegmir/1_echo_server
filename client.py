ad23de6bd198156cd003530d440450e8ce98b0e8
Grigoriy Dedenko <gdedenko@gmail.com>
2024-03-26 21:01:00 +0300

    added new port

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 import socket
 from time import sleep
 
+port=int(input("port:"))
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('localhost', 9090))
+sock.connect(('localhost', port))
 
 msg = input("Your string:")
 #msg = "Hi!"
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import socket
 
-port=9090
+port=int(input("port:"))
 sock = socket.socket()
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('', port))
