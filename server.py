00f378dc8253a6f9e501102151d89b80bdd59e83
Gendalf71 <gdedenko@gmail.com>
2024-10-05 11:14:43 +0300

    added details by point 2

--- server.py
+++ server.py
@@ -12,15 +12,15 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # Привязываем сокет к указанному порту на всех доступных интерфейсах (пустая строка означает любой доступный адрес)
 sock.bind(('', port))
 
-print("Server is starting")
+print("Server is starting") # пункт 2.i выполнен
 
 # Начинаем прослушивание входящих соединений (0 означает максимально возможное количество соединений)
 sock.listen(0)
-print("Port", port, "is listing")
+print("Port", port, "is listing") # пункт 2.ii выполнен
 
 # Принимаем входящее соединение. conn - новый сокет для общения с клиентом, addr - адрес клиента
 conn, addr = sock.accept()
-print("Client is accepted")
+print("Client is accepted") # пункт 2.iii выполнен
 print("Client adress:", addr[0])  # Выводим IP-адрес клиента
 print("Client port:", addr[1])    # Выводим порт клиента
 
@@ -30,13 +30,15 @@ while True:
     # Получаем данные от клиента (не более 1024 байт за раз)
     data = conn.recv(1024)
     if not data:   # Если данных больше нет, выходим из цикла
-        print("All data is accepted")
+        print("All data is accepted") # пункт 2.iv выполнен
         break
     msg += data.decode()  # Декодируем полученные данные и добавляем к сообщению
     # Отправляем обратно клиенту полученное сообщение в верхнем регистре
     conn.send(msg.upper().encode())
+    print("Message to client is sent") # пункт 2.v выполнен
 
 print(msg)  # Выводим полное полученное сообщение
 
 conn.close()  # Закрываем соединение с клиентом
-
+print("Connection is closed. Client is off") # пункт 2.vi выполнен
+print("Server is off") # пункт 2.vii выполнен
