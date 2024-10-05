5030c7523275d8945aac2c35b189d68e101b634f
Grigory Dedenko <gdedenko@gmail.com>
2024-10-05 10:48:01 +0300

    added comments into files

--- server.py
+++ server.py
@@ -1,27 +1,42 @@
-import socket
+import socket  # Импортируем модуль socket для работы с сетевыми соединениями
 
-port=int(input("port:"))
+# Запрашиваем у пользователя номер порта и преобразуем его в целое число
+port = int(input("port:"))
+
+# Создаем новый сокет
 sock = socket.socket()
+
+# Устанавливаем опцию сокета SO_REUSEADDR, чтобы можно было повторно использовать адрес
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Привязываем сокет к указанному порту на всех доступных интерфейсах (пустая строка означает любой доступный адрес)
 sock.bind(('', port))
+
 print("Server is starting")
+
+# Начинаем прослушивание входящих соединений (0 означает максимально возможное количество соединений)
 sock.listen(0)
-print("Port",port,"is listing")
+print("Port", port, "is listing")
+
+# Принимаем входящее соединение. conn - новый сокет для общения с клиентом, addr - адрес клиента
 conn, addr = sock.accept()
 print("Client is accepted")
-print("Client adress:",addr[0])
-print("Client port:",addr[1])
+print("Client adress:", addr[0])  # Выводим IP-адрес клиента
+print("Client port:", addr[1])    # Выводим порт клиента
 
-msg = ''
+msg = ''  # Инициализируем пустую строку для хранения полученного сообщения
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-            print("All data is accepted")
-            break
-	msg += data.decode()
-	conn.send(msg.upper().encode())
+    # Получаем данные от клиента (не более 1024 байт за раз)
+    data = conn.recv(1024)
+    if not   # Если данных больше нет, выходим из цикла
+        print("All data is accepted")
+        break
+    msg += data.decode()  # Декодируем полученные данные и добавляем к сообщению
+    # Отправляем обратно клиенту полученное сообщение в верхнем регистре
+    conn.send(msg.upper().encode())
 
-print(msg)
+print(msg)  # Выводим полное полученное сообщение
 
-conn.close()
+conn.close()  # Закрываем соединение с клиентом
+
